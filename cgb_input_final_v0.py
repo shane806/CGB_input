b28fd77f7c287106ccd4ac7d71abbadbcfdb6928
shane806 <47383634+shane806@users.noreply.github.com>
2020-03-31 13:47:36 -0400

    Update cgb_input_final_v0.py

--- cgb_input_final_v0.py
+++ cgb_input_final_v0.py
@@ -1,4 +1,3 @@
-#co-creators: Dr. Ivan Erill and Mr. Shane Humphrey
 """
 Reads in a JSON file containing at least five required elements:
 1. Transcription factor (TF)family name 
@@ -9,43 +8,77 @@ Reads in a JSON file containing at least five required elements:
 5. cgb_parameters which govern the scope of cgb
 
 Basic operation:
-(1) Using the provided TF protein accession numbers , cgb_input_generator 
-performs a BLASTP search for each TF, limited by e-value and (optionally) 
-by a taxonomic organism ID. (See input_test.md for details)
 
-If no taxonomic ID is specified, BLAST hits will be obtained up to a
-maximum specified e-value (1e-10 by default). If, however,  a taxonomic ID 
-is specified, the BLASTP search will be constrained to that taxon (e.g. class, order...) as
-well as by the specified e-value.
+TASK 1:
 
-Identified HSPs from BLASTP searches are consolidated into a list of
-unique hits, relative to each other, and to the TFs under study 
+  a. Using the provided TF protein accession numbers , cgb_input_generator 
+    performs a BLASTP search for each TF, limited by e-value and (optionally) 
+    by a taxonomic organism ID. (See input_test.md for details)
 
-# need to finish (2)
-(2) Once HSPs for all TFs have been returned, the next task is to identify 
-and retrieve the 'best' genome record to which the HSP belongs.
-- 'Best' is defined in greater detail in the 
-- To accomplish this, the Identical Protein Group (IPG) report for each HSP is retrieved
-with its protein accession number. 
-- The IPG report contains linked genome records which have been annotated with the
-HSP in their coding sequence (CDS) that codes for the HSP is identified
-from annotated genome records in the IPG report.then identifies all 
-genome records associated with filtered BLASTP hits by prioritizing, 
-, genomic records corresponding to
-complete genome assemblies. In the case of WGS records, it will seek the most
-complete WGS record (longest number of base pairs in the assembly).
+  b. If no taxonomic ID is specified, BLAST hits will be obtained up to a
+    maximum specified e-value (1e-10 by default). If, however,  a taxonomic ID 
+    is specified, the BLASTP search will be constrained to that taxon (e.g.
+    class, order...) well as by the specified e-value.
 
-(3) Once all corresponding contig records have been identified, the script offers
-three distinct operation modes:
-- report only one genome record per taxonomic level indicated by the user (e.g.
-  genus)
-- report only genome records for proteins with pair-wise identities below a
-  threshold provided by the user (e.g. 90% identity) (to be implemented)
-- report all identified genome records
+  c. Identified HSPs from BLASTP searches are consolidated into a list of
+    unique hits, relative to each other, and to the TFs under study 
 
-(4) Following the protocol indicated in (3), a cgb ready input file is generated
- and human readability is maintained to allow for post-hoc edits
+TASK 2:
 
+  a. Once HSPs for all TFs have been returned, the next task is to identify 
+    and retrieve the 'best' genome record to which the HSP belongs, and collect
+    the following coding sequence (CDS) data: 
+
+      1. genome acccession.version ID 
+      2. start position
+      3. stop position
+      4. strand directionality
+      5. p_score (defined in genome_record_retrieval() function)
+
+      * 'Best' is defined in detail in the genome_record_retrieval() function,
+       but generally, complete RefSeq genomes are most desired.
+
+  b. Once all pieces of information are collected, they are organized into 
+     a dictionary of dictionaries, where the main, 'outer', key is the protein
+     accession ID of the HSP, and all the genome and CDS information is stored
+     in a dictionary as the value for the HSP
+
+     ex.
+      { 'ZZ_123456.7' : { 'genome_accession' : 'AA_987654.3', 
+                          'start_pos : 1234,
+                          'stop_pos : 5678,
+                          'strand : '+',
+                          'p_score' : 7}}
+  c. Following genome retrieval, the Entrez database is then searched for any 
+    unidentified genome records such as plasmids for complete records and contig
+    records for WGS records
+
+TASK 3:
+
+  a. Following an exhaustive search for all relevant genome records for each 
+  HSP, the plasmid or contig record(s) are added to the corresponding HSP under
+  a new key called, 'genomes'.
+
+TASK 4:
+
+  a. - The last major task to complete is sampling HSPs
+     - There are three sampling methods from which the user may choose, and will
+       indicate which of the three method(s) in the input_parameters section
+       of the input file. (see test_input.md)
+
+       METHODS
+        1. Select only one genome record per taxonomic level (e.g. genus)
+
+        2. Select only records for HSPs with pair-wise promoter region 
+           identities below a user-specified threshold (e.g. 90% identity)
+           
+        3. Select all HSPs identified
+
+TASK 5:
+
+    a. Following the protocol indicated in TASK 4, all relevant information is
+      compiled into a JSON file which will be 100% cgb ready, and will still be
+       human readable to allow for post-hoc edits.
 """
 
 # Make sure you have: biopython and urllib2 installed in your environment
