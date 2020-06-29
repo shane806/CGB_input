ba7bb9cad623214e338e59070934b7304b685abd
shane806 <47383634+shane806@users.noreply.github.com>
2020-06-28 22:30:27 -0400

    Update cgb_input_run.py

--- cgb_input_run.py
+++ cgb_input_run.py
@@ -1,3 +1,3 @@
-from cgb_input.cgb_input import cgb_input_main
+from cgb_input import cgb_input_main
 
-input_file = cgb_input_main('GcrA_Alphaproteobacteria_ctrl_input.json')
+input_file = cgb_input_main('your_json_file.json')
